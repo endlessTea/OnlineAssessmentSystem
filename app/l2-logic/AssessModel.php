501c9411817fe238ebcf5abf8324c483eb5c1db7
Goliath <melondev50@gmail.com>
2015-08-02 09:47:51 +0100

    amends to assessmodel

--- app/l2-logic/AssessModel.php
+++ app/l2-logic/AssessModel.php
@@ -7,12 +7,15 @@
  */
 class AssessModel {
 
+  # treat conceptually similarly to UserModel:
+  # store test document as instance variable
+
   // USER IS READY TO TAKE A TEST
 
   // USER SUBMITS AN ANSWER TO A QUESTION
 
   // USER COMPLETES A TEST AND SYSTEM ACKNOWLEDGES
-  
+
   // SYSTEM DELIVERS FEEDBACK FOR A QUESTION
 
 }
